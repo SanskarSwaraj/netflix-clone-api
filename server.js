a54bf35a017e0f2b9a864909c124f50789b01b37
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2023-03-13 23:08:52 +0530

    fix: env vars

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes = require("./routes/UserRoutes")
+const userRoutes = require("./routes/UserRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://sanskarswaraj:sanskarswaraj@netflix-cluster.4foo4zf.mongodb.net/test?authMechanism=SCRAM-SHA-1", {
+const dbUri = process.env.DB_URI;
+
+mongoose
+  .connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-.then(() => {
+  .then(() => {
     console.log("DB Connected");
-});
+  });
 
 app.use("/api/user", userRoutes);
 
-app.listen(4000, console.log("server started"));
+app.listen(4000, () => console.log("server started"));
